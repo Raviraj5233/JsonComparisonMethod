2d5537e0794b7a800fc6a6b171ef45d76a90945f
Raviraj5233 <raviraj@qable.io>
2022-12-01 11:30:42 +0530

    Json Comparison using gson and guava library

---
+++ src/test/java/myTest.java
@@ -0,0 +1,65 @@
+
+import com.google.common.collect.MapDifference;
+import com.google.common.collect.Maps;
+import com.google.gson.Gson;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
+import com.google.gson.reflect.TypeToken;
+import java.io.FileReader;
+import java.lang.reflect.Type;
+import java.util.Map;
+
+public class myTest {
+
+    public static void main(String[] args) {
+        JsonElement Json1=getJson1();
+        JsonElement Json2=getJson2();
+
+        Gson g = new Gson();
+        Type mapType = new TypeToken<Map<String, Object>>() {
+        }.getType();
+        Map<String, Object> firstMap = g.fromJson(Json1, mapType);
+        Map<String, Object> secondMap = g.fromJson(Json2, mapType);
+        MapDifference<String, Object> difference = Maps.difference(firstMap, secondMap);
+
+        System.out.println("\n\nEntries are missing in Json2\n--------------------------\n");
+        difference.entriesOnlyOnLeft().forEach((key, value) ->
+                System.out.println("--> " + key + ": " + value));
+
+        System.out.println("\n\nEntries are missing in Json1\n--------------------------\n");
+        difference.entriesOnlyOnRight().forEach((key, value) ->
+                System.out.println("--> " + key + ": " + value));
+
+        System.out.println("\n\nEntries Mismatching:\n--------------------------\n");
+        difference.entriesDiffering().forEach((key, value) ->
+                System.out.println("--> " + key + ": " + value));
+
+        System.out.println("\n\nEntries Common:\n--------------------------\n");
+        difference.entriesInCommon().forEach((key, value) ->
+                System.out.println("--> " + key + ": " + value));
+    }
+    public static JsonElement getJson1() {
+        JsonParser jsonParser = new JsonParser();
+        JsonElement jsonElement=null;
+        try {
+            FileReader fileReader = new FileReader("C:\\Users\\Raviraj\\Desktop\\JsonComparisonTask\\src\\main\\java\\jsonfiles\\actualJson.json");
+            jsonElement = jsonParser.parse(fileReader);
+        } catch (Exception e) {
+            System.out.println("File not found");
+        }
+        return jsonElement;
+    }
+    public static JsonElement getJson2() {
+        JsonParser jsonParser = new JsonParser();
+        JsonElement jsonElement=null;
+        try {
+            FileReader fileReader = new FileReader("C:\\Users\\Raviraj\\Desktop\\JsonComparisonTask\\src\\main\\java\\jsonfiles\\expectedJson.json");
+            jsonElement = jsonParser.parse(fileReader);
+        } catch (Exception e) {
+            System.out.println("File not found");
+        }
+        return jsonElement;
+    }
+}
+
+
